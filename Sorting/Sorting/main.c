6870e20762e0f6eb21a296cc01f85f8f6c975b78
hazelmelis <hazelerkmen14@gmail.com>
2025-03-16 16:49:09 -0400

    Fix bubble sort test

--- Sorting/Sorting/main.c
+++ Sorting/Sorting/main.c
@@ -4,9 +4,9 @@ void bubble_sort(int* a, int size);
 
 int main(int argc, char* argv[])
 {
-	int a[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	int a[] = { 38, 5, 34, 23, 69, 0, 1, 5, 6, 4, 14 };
 
-	bubble_sort(a, 10);
+	bubble_sort(a, sizeof(a)/sizeof(a[0]));
 
 	for (int i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
 	{
@@ -18,9 +18,9 @@ int main(int argc, char* argv[])
 
 void bubble_sort(int* a, int size)
 {
-	for (int i = 0; i < size; ++i)
+	for (int i = 0; i < size - 1; ++i)
 	{
-		for (int j = 0; j < size - 1; ++j)
+		for (int j = 0; j < size - 1 - i; ++j)
 		{
 			if (a[j] > a[j + 1])
 			{
